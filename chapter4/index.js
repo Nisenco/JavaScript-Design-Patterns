ee48832daa96c126dde1d74dc9544b4e0c7abcb1
Nicosen <SUCCESSW2M@163.com>
2020-10-16 19:20:27 +0800

    chapter 5 章节

--- chapter4/index.js
+++ chapter4/index.js
@@ -22,3 +22,105 @@ console.log(a === b);
 
 // 透明的单例模式
 
+var CreateDiv = (function(){
+	var instance;
+	var CreateDiv = function (html){
+		if(instance){
+			return instance;
+		}
+		this.html = html;
+		this.init();
+		return instance = this;
+	}
+	CreateDiv.prototype.init = function(){
+		var div = document.createElement('div');
+		div.innerHTML = this.html;
+		document.body.appendChild(div);
+	}
+	return CreateDiv;
+})();
+
+var a = new CreateDiv('sven1');
+var b = new CreateDiv('sven2');
+console.log(a=== b);
+
+// 用代理实现 单例模式
+
+var CreateDiv = (function(){
+	var instance;
+	
+	return CreateDiv;
+})();
+var CreateDiv = function (html){
+	this.html = html;
+	this.init();
+}
+CreateDiv.prototype.init = function(){
+	var div = document.createElement('div');
+	div.innerHTML = this.html;
+	document.body.appendChild(div);
+}
+
+//  引入代理类 proxySingletonCreateDiv
+
+var proxySingletonCreateDiv = (function(){
+	var instance;
+	return function(html){
+		if(!instance){
+			instance = new CreateDiv(html);
+		}
+		return instance;
+	}
+})();
+var a = new proxySingletonCreateDiv('sven1');
+var b = new proxySingletonCreateDiv('sven2');
+console.log(a=== b);
+
+// javascript 中的单例模式
+//  单例模式的核心的 确保只有一个实例，并提供全局访问。
+
+// 降低全局变量带来的命名污染的几种方式
+// 1 使用命名空间
+// demo 1 
+var nameSpace1 = {
+	a:function (){
+		console.log(1);
+	}
+}
+// 2 使用闭包封装私有变量
+var user = (function(){
+	var _name = 'seven';
+	var _age = '26';
+	return {
+		getUserInfo:function(){
+			return _name +'--' + _age;
+		}
+	}
+})()
+
+// 惰性单例
+// 惰性单例是指在需要的时候才创建对象
+var Singleton = function(name){
+	this.name = name;
+}
+Singleton.prototype.getName = function (){
+	console.log(this.name);
+}
+Singleton.getInstance = (function(){
+	var instance;
+	return function (name){
+		if(!instance){
+			instance = new Singleton(name);
+		}
+		return instance;
+	}
+})()
+
+// 通用的惰性单例
+
+var getSingle = function(fn){
+	var result;
+	return function(){
+		return result || (result = fn.apply(this,arguments));
+	}
+}
---
+++ chapter5/index.js
@@ -0,0 +1,190 @@
+// 策略模式
+// 策略模式是指 定义一系列的算法，把他们一个个的封装起来，并且使他们可以互相替换
+//  一个基于策略模式实现的程序至少由两部分组成。
+// 第一个部分是一组策略类，策略类封装了具体的算法，并负责具体的计算过程
+// 第二部分是环境类（Context），context接受客户的请求，随后把请求委托给某一个策略类。
+// 
+
+// 基于传统面向对象语言的模仿策略模式
+var performanceS = function(){};
+performanceS.prototype.calculate = function(salary){
+	return salary*4;
+}
+var performanceA = function(){};
+performanceA.prototype.calculate = function(salary){
+	return salary*3;
+}
+var performanceB = function(){};
+performanceB.prototype.calculate = function(salary){
+	return salary*2;
+}
+
+// 定义奖金类
+var Bonus = function (){
+	this.salary = null; // 原始工资
+	this.strategy = null; // 绩效等级对应的策略对象
+}
+Bonus.prototype.setSalary = function (salary){
+	this.salary = salary;
+}
+Bonus.prototype.setSrategy = function (strategy){
+	this.strategy = strategy;
+}
+Bonus.prototype.getBouns = function (){
+	return this.strategy.calculate(this.salary); // 把计算的奖金操作委托给对应的策略对象
+}
+
+var bonus = new Bonus();
+bonus.setSalary(1000);
+bonus.setSrategy(new performanceS());
+console.log(bonus.getBouns())
+
+// JavaScript 版本的策略模式
+
+var strategies = {
+	'S':function (salary){
+		return salary * 4;
+	},
+	'A':function (salary){
+		return salary * 3;
+	},
+	'B':function (salary){
+		return salary * 2;
+	}
+}
+var calculateBonus = function(level,salary){
+	return strategies[level](salary);
+}
+console.log(calculateBonus('S',3000));
+
+//  使用策略模式实现缓动动画
+
+var tween = {
+	linear:function (t,b,c,d){
+		return c*t/d + b;
+	},
+	easeIn:function(t,b,c,d){
+		return c*(t /= d)*t +b;
+	},
+	strongEaseIn:function(t,b,c,d){
+		return c * ( t /= d ) * t * t * t * t + b;
+	},
+	strongEaseOut: function(t, b, c, d){
+		return c * ( ( t = t / d - 1) * t * t * t * t + 1 ) + b;
+	},
+	sineaseIn: function( t, b, c, d ){
+		return c * ( t /= d) * t * t + b;
+	},
+	sineaseOut: function(t,b,c,d){
+		return c * ( ( t = t / d - 1) * t * t + 1 ) + b;
+	}
+}
+var Animate = function (dom){
+	this.dom = dom; // 进行运动的DOm 节点
+	this.startTime = 0; // 动画开始的时间
+	this.startPos = 0; // 动画开始时 dom 节点的位置，即 dom 的初始位置
+	this.endPos = 0;   // 动画结束时 dom 节点的位置，即 dom 的目标位置
+	this.propertyName = null; // dom 节点需要被改变的属性
+	this.easing = null; // 缓动算法
+	this.duration = null; // 动画持续时间
+}
+// propertyName：要改变的 CSS 属性名，比如'left'、 'top'，分别表示左右移动和上下移动
+// endPos： 小球运动的目标位置。
+// duration： 动画持续时间
+// easing： 缓动算法
+Animate.prototype.start = function(propertyName,endPos,duration,easing){
+	this.startTime = +new Date;
+	this.startPos = this.dom.getBoundingClientRect()[propertyName] ;// dom 节点初始位置
+	this.propertyName = propertyName;
+	this.endPos = endPos;
+	this.duration = duration;
+	this.easing = tween[easing];
+
+	var self = this;
+	var timeId = setInterval(function() {
+		if(self.step() === false){
+			clearInterval(timeId);
+		}
+	}, 19)
+}
+Animate.prototype.step = function(){
+	var t = +new Date;
+	if(t >= this.startTime + this.duration){
+		this.update(this.endPos);
+		return false;
+	}
+	var pos  = this.easing(t-this.startTime,this.startPos,this.endPos-this.startPos,this.duration);
+	this.update(pos);
+}
+
+Animate.prototype.update = function(pos){
+	this.dom.style[this.propertyName] = pos + 'px';
+}
+
+// var div = document.getElementById( 'div' );
+// var animate = new Animate( div );
+// animate.start( 'left', 500, 1000, 'strongEaseOut' );
+
+//  表单校验
+
+var strategies = {
+	isNonEmpty:function(value,errorMsg){
+		if(value ==''){
+			return errorMsg;
+		}
+	},
+	minLength:function(value,length,errorMsg){
+		if(value.length < length){
+			return errorMsg;
+		}
+	},
+	isMobile:function(value,errorMsg){
+		if(!/(1[3|5|8][0-9]{9}$)/.test(value)){
+			return errorMsg;
+		}
+	}
+}
+
+var validataFunc = function(){
+	// 校验器
+	var validator = new Validator();
+	// 添加校验规则
+	validator.add(registerForm.username,'isNonEmpty','用户名不能为空');
+	validator.add(registerForm.password,'minLength:6','密码长度不能少于6位');
+	validator.add(registerForm.phoneNumber,'isMobile','手机号格式不正确');
+	var errorMsg = validator.start();
+	return errorMsg;
+}
+
+var registerForm = document.getElementById('registerForm');
+registerForm.onsubmit = function(event){
+	var errorMsg = validataFunc();
+	event.preventDefault();
+	return false;
+	// if(errorMsg){
+	// 	console.log(errorMsg);
+	// 	return false;
+	// }
+}
+
+var Validator = function(){
+	this.cache = []; // 保存校验规则
+};
+Validator.prototype.add = function(dom,rule,errorMsg){
+	var ary = rule.split(':');
+	this.cache.push(function(){
+		var strategy = ary.shift();
+		ary.unshift(dom.value);
+		ary.push(errorMsg);
+		console.log(ary,'----errorMsg-----');
+		return strategies[strategy].apply(dom,ary)
+	});
+}
+Validator.prototype.start = function(){
+	for(var i = 0, validataFunc; validataFunc= this.cache[i++];){
+		var msg = validataFunc();
+		if(msg){
+			return msg;
+		}
+	}
+} 
